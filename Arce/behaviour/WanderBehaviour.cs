f19ff7325f38797bec2b9c8e85bc5def15ab5be8
Thomas Vuister (student) <s1119966@student.windesheim.nl>
2020-02-07 12:44:48 +0100

    Added wanderbehaviour, but doens't work

--- Arce/behaviour/WanderBehaviour.cs
+++ Arce/behaviour/WanderBehaviour.cs
@@ -9,12 +9,27 @@ namespace Arce.behaviour
 {
     class WanderBehaviour : SteeringBehaviour
     {
+        double wanderRadius = 25;
+        double wanderDistance = 50;
+        double wanderJitter = 5;
+
         public WanderBehaviour(MovingEntity me) : base(me)
         {
         }
 
         public override Vector2D Calculate()
         {
+            Random random = new Random();
+            Vector2D wanderTarget = new Vector2D(random.Next(-1, 1) * wanderJitter, random.Next(-1, 1) * wanderJitter);
+
+            wanderTarget.Normalize();
+            wanderTarget = wanderTarget.Multiply(wanderRadius);
+
+            Vector2D targetLocal = wanderTarget.Add(new Vector2D(wanderDistance, 0));
+
+            // to do
+
+            return targetLocal.Sub(ME.Pos);
             
         }
     }
