6af4baf46a6a9cd565380dd4be934b8bd9175951
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-02-11 12:12:40 +0100

    AlignmentBehaviour added

---
+++ Arce/behaviour/AlignmentBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using Arce.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arce.behaviour
+{
+    class AlignmentBehaviour : SteeringBehaviour
+    {
+        private Vector2D averageHeading;
+        private int neighbourCount = 0;
+
+        public AlignmentBehaviour(MovingEntity movingEntity) : base(movingEntity)
+        {
+        }
+
+        public override Vector2D Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
