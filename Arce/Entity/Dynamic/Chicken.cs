7f233e8f9d0b0a62eb83134d9463cc763b5337d9
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-03-01 16:44:15 +0100

    Added a chicken

---
+++ Arce/Entity/Dynamic/Chicken.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arce.Entity.Dynamic
+{
+    class Chicken : DynamicGameEntity
+    {
+        public Chicken(Vector2 pos) : base(pos)
+        {
+            Mass = 1.0F;
+            MaxSpeed = 20.0F;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
