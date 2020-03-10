0c00e6a931b3a9cd8a27b789a40d34909830bf94
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-03-10 15:38:29 +0100

    Backtracking base code

---
+++ Lecture05 Backtracking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lecture05_Backtracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NQueens nQueens = new NQueens(4);
+            nQueens.SolveBacktracking();
+        }
+    }
+}
